f84475c8ede93929a6095391ef286361f0d99461
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-12 18:51:06 -0300

    pegando dados da página

--- scraper_citacoes.py
+++ scraper_citacoes.py
@@ -1,7 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+# requisição para página
+pagina = requests.get('https://quotes.toscrape.com/')
+# transformando em um html
+dados_pagina = BeautifulSoup(pagina.text,'html.parser')
+# melhorando estrutura html da página
+print(dados_pagina.prettify())
+
 def main():
     ...
+
+def pagina():
+    ...    
 
 main()
